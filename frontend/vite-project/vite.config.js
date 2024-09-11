d5cb542345caaafeff8d69f20e9e39b5f045a94a
lee <mcli2023@outlook.com>
2024-09-11 13:49:38 +0100

    Update configuration for Render deployment

--- frontend/vite-project/vite.config.js
+++ frontend/vite-project/vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["socket.io-client"],
+  build: {
+    outDir: "dist",
   },
 });
